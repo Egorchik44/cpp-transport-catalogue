484646fce58b62915c5c894498642e0cf06c6c27
Egorchik44 <111063360+Egorchik44@users.noreply.github.com>
2023-07-09 17:21:07 +0300

    Sprint #12 review #1

--- transport-catalogue/request_handler.cpp
+++ transport-catalogue/request_handler.cpp
@@ -3,22 +3,22 @@
 void RequestHandler::ProcessRequests(const json::Node& stat_requests) const {
     json::Array result;
     for (auto& request : stat_requests.AsArray()) {
-        const auto& request_map = request.AsMap();
+        const auto& request_map = request.AsDict();
         const auto& type = request_map.at("type").AsString();
         Print printer{};
         if (type == "Bus")
         {
-            result.push_back(printer.RendererPrintRoute(catalog_, request_map).AsMap());
+            result.push_back(printer.RendererPrintRoute(catalog_, request_map).AsDict());
         }
         
         if (type == "Stop")
         {
-            result.push_back(printer.RendererPrintStop(catalog_, request_map).AsMap());
+            result.push_back(printer.RendererPrintStop(catalog_, request_map).AsDict());
         }
         
         if (type == "Map") 
         { 
-            result.push_back(renderer_.RendererPrintMap(catalog_, request_map).AsMap());
+            result.push_back(renderer_.RendererPrintMap(catalog_, request_map).AsDict());
         }
     }
 
